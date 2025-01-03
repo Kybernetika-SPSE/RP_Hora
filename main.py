a983166d5ed8e558f9e851f378222b46e9fad84c
malejkopecek1 (O.H.) <119786317+malejkopecek@users.noreply.github.com>
2025-01-03 17:31:39 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import network
+import urequests
+import os
+from time import sleep
+from machine import Pin  
+
+led = Pin(2, Pin.OUT)
+
+for _ in range(5):
+    led.on()
+    sleep(0.2)
+    led.off()
+    sleep(0.2)
